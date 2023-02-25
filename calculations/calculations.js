f636e9578c4ebeee05d8958a0ef9d261337169ec
KotovNikita16 <kotoff123@gmail.com>
2023-02-26 00:30:48 +0600

    learnjs-homework

--- calculations/calculations.js
+++ calculations/calculations.js
@@ -19,4 +19,7 @@ function lDiv(x, y, ...others) {
 let x = 465468784654687583475634756347563475637456534756534756347568537567346573468573n;
 let z = 5180177860891957963178911808109081090007109110709919812109276947889999758684891;
 
-console.log(x + "\n" + lSum(x, z, 2, 2) );
+console.log(x + "\n" + lSum(x, z, 2, 2) + "\n");
+console.log(x + "\n" + lMul(x, z, 2, 2) + "\n");
+console.log(x + "\n" + lSub(x, z, 2, 2) + "\n");
+console.log(x + "\n" + lDiv(x, 100) );
